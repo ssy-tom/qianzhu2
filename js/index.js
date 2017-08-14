7e1557eb3eee94eb3a52fcbfbea7745e2c3837f8
Kang <1310814423@qq.com>
2017-08-14 21:15:13 +0800

    杨康第二屏修改

--- js/index.js
+++ js/index.js
@@ -3,29 +3,16 @@
 		//全屏滚动块
 		screenScrolling()
 		function screenScrolling(){
-				let box = document.getElementById('box');
-				let openBox = document.getElementById('openBox')
-				let Bbox = new Cboxc();
-				Bbox.ChuShi(openBox,box)
+			var box = document.getElementById('box');
+			var Bbox = new Cboxc();
+			Bbox.ChuShi(box)
 				function Cboxc(){
-					this.ChuShi = function(openBox,box){
-						this.openBox = openBox;
-						this.openBoxs = this.openBox.children;
+					this.ChuShi = function (box) {
 						this.box = box;
 						this.boxs = this.box.children;
 						this.num = 0;
-						this.Create();
-						this.ClickNum();
-						this.StyleBack();
 						this.lungun();
 						this.ifOpen();
-					}
-					this.Create = function(){
-						for (let i = 0; i < this.boxs.length; i++) {
-							let Inner = document.createElement('a');
-							Inner.className = 'active';
-							this.openBox.appendChild(Inner);
-						};
 					}
 					this.IfNum = function(tra){
 						if (tra == true) {
@@ -35,7 +22,6 @@
 							}else{
 								return ;
 							}
-							this.StyleBack();
 						}else{
 							if (this.num > 0) {
 								this.num --;
@@ -43,26 +29,7 @@
 							}else{
 								return ;
 							}
-							this.StyleBack();
 						}
-					}
-					this.StyleBack = function(){
-						this.openBoxs[this.num].style.background = 'red';
-						[...this.openBoxs].forEach(function(value,index){
-							value.style.background = '';
-						})
-						this.openBoxs[this.num].style.background = 'red';
-					}
-
-					this.ClickNum = function(){
-						this.openBox.addEventListener('click',function(e){
-							if(e.target.className == 'active'){
-								console.log(this.num)
-								this.num = [...this.openBoxs].indexOf(e.target);
-								this.StyleBack();
-								this.box.style.transform = 'translate3d(0px,'+-this.num*100+'%, 0px)';
-							}
-						}.bind(this),false)
 					}
 					this.Remove = function(event){
 						if(event.deltaY < 0){
@@ -82,9 +49,13 @@
 						window.removeEventListener("mousewheel",this.Remove,false);
 					}
 					this.ifOpen = function(){
-						this.openBox.addEventListener('webkitTransitionEnd',function(){this.lungun()}.bind(this),false)
+						this.box.addEventListener('webkitTransitionEnd', function () {
+							this.lungun()
+						}.bind(this), false)
 					}
 				}
 		}
+
+
 	}
 })(window)
