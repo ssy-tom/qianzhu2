e358fcf1ef37829a8068ea1b8f46535c84a3514a
赵佳佳 <1528220721@qq.com>
2017-08-15 08:23:29 +0800

    赵佳佳

--- js/index.js
+++ js/index.js
@@ -4,21 +4,26 @@
 		screenScrolling()
 		function screenScrolling(){
 				let box = document.getElementById('box');
-				let openBox = document.getElementById('openBox')
+				let openBox = document.getElementById('openBox');
+				//赵佳佳 第一屏下箭头点击
+				var btnOne=document.getElementsByClassName("btnOne")[0];
 				let Bbox = new Cboxc();
-				Bbox.ChuShi(openBox,box)
+				Bbox.ChuShi(openBox,box,btnOne)
 				function Cboxc(){
-					this.ChuShi = function(openBox,box){
+					this.ChuShi = function(openBox,box,btnOne){
 						this.openBox = openBox;
 						this.openBoxs = this.openBox.children;
 						this.box = box;
 						this.boxs = this.box.children;
 						this.num = 0;
+						this.btnOne=btnOne;
 						this.Create();
 						this.ClickNum();
 						this.StyleBack();
 						this.lungun();
 						this.ifOpen();
+						//赵佳佳 第一屏点击下箭头
+						this.clickOne();
 					}
 					this.Create = function(){
 						for (let i = 0; i < this.boxs.length; i++) {
@@ -84,7 +89,112 @@
 					this.ifOpen = function(){
 						this.openBox.addEventListener('webkitTransitionEnd',function(){this.lungun()}.bind(this),false)
 					}
+
+					//赵佳佳 第一屏点击下箭头
+					this.clickOne=function(){
+						this.btnOne.addEventListener("click",function(){
+							this.num=1;
+							this.box.style.transform = 'translate3d(0px,'+-this.num*100+'%, 0px)';
+						}.bind(this),false)
+					}
+
 				}
+		}
+
+		//赵佳佳 第一屏轮播
+		NumOneLunbo();
+		function NumOneLunbo(){
+			var bgBox=document.getElementById("bgBox");
+			var homepageLunbo=document.getElementsByClassName("homepageLunbo");
+			var cw=document.body.clientWidth;
+			bgBox.style.width=cw*(homepageLunbo.length)+"px";
+			var LunBoNum=0;
+			var indexNum=0;
+			var furIndex=0;
+			var time;
+			var LunboBtn=document.getElementsByClassName("LunboBtn");
+			// 获取第一个子元素
+			function getFirstChild(obj){
+				return obj.children[0];
+			}
+			function getLastChild(obj){
+				return obj.children[obj.children.length-1];
+			}
+			for (var i = 0; i < homepageLunbo.length; i++) {
+				homepageLunbo[i].style.width=cw+'px'
+			};
+
+			function move(){
+				LunBoNum++;
+				if (LunBoNum>LunboBtn.length-1) {
+					LunBoNum=0;
+				};
+				animate(bgBox,{left:-cw},function(){
+					var fir=bgBox.children[0];
+					bgBox.appendChild(fir);
+					bgBox.style.left=0
+
+					for(var j=0;j<LunboBtn.length;j++){
+						LunboBtn[j].style.background=""
+					}
+					LunboBtn[LunBoNum].style.background="#00DFB9"
+				})
+			}
+			function moveLunBo2(){
+				time=setInterval(move,2000);
+			}
+			function clearLunBo2(){
+				clearInterval(time);
+			}
+			moveLunBo2();
+			var LunboBtn=document.getElementsByClassName("LunboBtn");
+			function hoverBtn(){
+				for(var i=0;i<LunboBtn.length;i++){
+					LunboBtn[i].index=i;
+					LunboBtn[i].onmouseover=function(){
+						clearLunBo2();
+						indexNum=LunBoNum;
+						furIndex=this.index;
+						LunBoNum=this.index;
+						for(var j=0;j<LunboBtn.length;j++){
+							LunboBtn[j].style.background=""
+						}
+						LunboBtn[LunBoNum].style.background="#00DFB9";
+
+						var cha=furIndex-indexNum;
+						if (cha>0) {
+							animate(bgBox,{left:-cw},function(){
+								var fir=bgBox.children[0];
+								bgBox.appendChild(fir);
+								bgBox.style.left=0;
+								indexNum=furIndex;
+							})
+						}else{
+							var abs=Math.abs(cha);
+							bgBox.style.left=-cw*abs+"px";
+							for(var i=0;i<LunboBtn.length;i++){
+								// animate(bgBox,left:0,)
+								var fir2=getFirstChild(bgBox);
+								var last=getLastChild(bgBox);
+								bgBox.insertBefore(last,fir2);
+							}
+							animate(bgBox,{left:0},function(){
+								indexNum=furIndex;
+							})
+						}
+						moveLunBo2()
+					}
+				}
+			}
+			hoverBtn();
+		}
+		//改变屏幕大小
+		window.onresize=function() {
+			var homepageLunbo = document.getElementsByClassName("homepageLunbo");
+			for (var i = 0; i < homepageLunbo.length; i++) {
+				homepageLunbo[i].style.width = window.innerWidth + "px";
+
+			}
 		}
 	}
 })(window)
